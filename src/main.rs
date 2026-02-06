1b2fcc0ee9a078e8973dc059a6e52e683d33ff9f
nthree <147289374+nw31s@users.noreply.github.com>
2026-02-07 00:13:36 +0900

    2進数変換

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,53 @@
 use dashu::base::SquareRoot;
 use dashu::float::{FBig, round::mode::HalfEven};
-use std::fs;
-use std::io::Write;
 use std::time::Duration;
 use std::time::Instant;
 
-type BigFloat = FBig<HalfEven, 10>;
+type StrechFloat = FBig<HalfEven, 2>;
 
 fn main() {
     // 計算する桁数
-    let digits = 1_040_000;
+    let digits = 1040000;
 
     // 必要なビット精度を計算
-    // 桁数 + 100(誤差補正用)
-    let precision = digits + 100;
+    // 桁数 * log2(10) + 10(誤差補正用)
+    let precision = (digits as f64 * 10.0_f64.log2() + 10.0) as usize;
 
-    // 必要なループ回数を計算
-    // Gauss-Legendre法は2次収束なので、だいたい log2(precision) 回繰り返せば十分
-    let iterations = ((precision as f64).log2().ceil() as usize).max(10);
+    let start = Instant::now();
+    let _ = calculate_pi(precision);
+    let duration = start.elapsed();
 
-    println!("{}桁の計算開始．{}回のループとなります", digits, iterations);
-    let (pi, duration) = gauss_legendre_algorithm(precision, iterations);
-    println!(
-        "{} 桁の円周率計算を完了しました(実行時間: {:?})",
-        digits, duration
-    );
-
-    // 結果をファイルに保存
-    let mut file = fs::File::create("pi.txt").expect("ファイルの作成に失敗しました");
-    let pi_str = pi.to_string();
-    file.write_all(pi_str.as_bytes())
-        .expect("ファイルへの書き込みに失敗しました");
-    eprintln!("結果を pi.txt に保存しました");
+    println!("Calculated {} digits of pi in {:?}", digits, duration);
 }
 
 /// Gauss-Legendre法で円周率を計算する関数
 /// https://qiita.com/matsuda_tkm/items/418588d3c59cc8d85ec7
 /// https://ja.wikipedia.org/wiki/ガウス＝ルジャンドルのアルゴリズム
-fn gauss_legendre_algorithm(precision: usize, iterations: usize) -> (BigFloat, Duration) {
+fn calculate_pi(precision: usize) -> (StrechFloat, Duration) {
     let start = Instant::now();
     // 初期値を設定
-    let one = BigFloat::ONE.with_precision(precision).unwrap();
-    let two = BigFloat::from(2u8).with_precision(precision).unwrap();
-    let four = BigFloat::from(4u8).with_precision(precision).unwrap();
+    let one = StrechFloat::ONE.with_precision(precision).value();
+    let two = StrechFloat::from(2u8).with_precision(precision).value();
+    let four = StrechFloat::from(4u8).with_precision(precision).value();
 
     let mut a = one.clone();
-    let mut b = (&one / &two.sqrt()).with_precision(precision).unwrap();
+    let mut b = (&one / &two.sqrt()).with_precision(precision).value();
     let mut t = &one / &four;
     let mut p = one.clone();
 
-    eprintln!("初期値の計算が終了しました: {:?}", start.elapsed());
+    // 2次収束なので だいたいlog2(precision) 回繰り返せば十分
+    let iterations = ((precision as f64).log2().ceil() as u32).max(10);
 
     for i in 0..iterations {
-        let a_next = ((&a + &b) / &two).with_precision(precision).unwrap();
-        let b_next = (&a * &b).sqrt().with_precision(precision).unwrap();
+        let a_next = ((&a + &b) / &two).with_precision(precision).value();
+        let b_next = (&a * &b).sqrt().with_precision(precision).value();
         let a_diff = &a - &a_next;
         let t_next = &t - &(&p * &a_diff * &a_diff);
 
         a = a_next;
         b = b_next;
         t = t_next;
-        p = (&p * &two).with_precision(precision).unwrap();
+        p = (&p * &two).with_precision(precision).value();
 
         let elapsed = start.elapsed();
 
